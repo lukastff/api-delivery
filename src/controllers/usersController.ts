763a8661f65e18fd4d9c579bd7b2e0d59b70448d
lukastff <lucastavaresffr@gmail.com>
2025-10-21 10:19:34 -0300

    feat: implement user registration in database

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/database/prisma";
 import { hash } from "bcrypt";
 import { z } from "zod";
+import { AppError } from "@/utils/AppError";
 
 class UsersController {
   async create(req: Request, res: Response, next: NextFunction) {
@@ -12,9 +14,29 @@ class UsersController {
 
     const { name, email, password } = bodySchema.parse(req.body);
 
+    const userWithSameEmail = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists");
+    }
+
     const hashedPassword = await hash(password, 8);
 
-    return res.json({ message: "ok", hashedPassword });
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return res.json(userWithoutPassword);
   }
 }
 
