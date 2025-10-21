2b3e72423b31329fad681592fef50d8c836b556b
lukastff <lucastavaresffr@gmail.com>
2025-10-21 10:36:50 -0300

    feat: add authenticated user data fetching

--- src/controllers/sessionsController.ts
+++ src/controllers/sessionsController.ts
@@ -1,7 +1,30 @@
 import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/database/prisma";
+import { compare } from "bcrypt";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
 
 class SessionsController {
   async create(req: Request, res: Response, next: NextFunction) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string(),
+    });
+
+    const { email, password } = bodySchema.parse(req.body);
+
+    const user = await prisma.user.findFirst({ where: { email } });
+
+    if (!user) {
+      throw new AppError("Email or Password incorrect", 401);
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError("Email or Password incorrect", 401);
+    }
+
     return res.json({ message: "Hello World" });
   }
 }
