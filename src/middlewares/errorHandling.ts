e08df84d972cf99c92e8e6123da8ba02e1714e63
lukastff <lucastavaresffr@gmail.com>
2025-10-20 21:16:00 -0300

    feat: add zod validation and exception handling

--- src/middlewares/errorHandling.ts
+++ src/middlewares/errorHandling.ts
@@ -1,5 +1,6 @@
 import { AppError } from "@/utils/AppError";
 import { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
 
 export function errorHandling(
   error: any,
@@ -9,6 +10,13 @@ export function errorHandling(
 ) {
   if (error instanceof AppError) {
     res.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    res.status(400).json({
+      message: "Validation error",
+      issues: error.format(),
+    });
   }
 
   res.status(500).json({ message: error.message });
