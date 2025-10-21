be41f61230031fed38d03cd3a71eda8ee970819e
lukastff <lucastavaresffr@gmail.com>
2025-10-21 18:02:48 -0300

    feat: implement delivery log registration with validation

--- src/controllers/deliveryLogsController.ts
+++ src/controllers/deliveryLogsController.ts
@@ -1,10 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
 import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 
 class DeliveryLogsController {
   async create(req: Request, res: Response, next: NextFunction) {
-    return res.json({ message: "ok" });
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(req.body);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
+
+    if (!delivery) {
+      throw new AppError("Delivery not found", 404);
+    }
+
+    if (delivery.status === "processing") {
+      throw new AppError("Change status to shipped");
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return res.status(201).json();
   }
 }
 
