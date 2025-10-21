c75c3789636d117bed2f3af1e8472e089c55b5e5
lukastff <lucastavaresffr@gmail.com>
2025-10-21 17:08:38 -0300

    feat: implement delivery status update with validation

--- src/routes/deliveriesRoutes.ts
+++ src/routes/deliveriesRoutes.ts
@@ -3,11 +3,15 @@ import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
 import { DeliveriesController } from "@/controllers/deliveriesController";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
+import { DeliveriesStatusController } from "@/controllers/deliveriesStatusController";
+
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.get("/", deliveriesController.index);
 deliveriesRoutes.post("/", deliveriesController.create);
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveriesRoutes };
